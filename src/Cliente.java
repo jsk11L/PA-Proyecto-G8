be198d8506fa56350db14b5037c05f2899b49f80
Francisca Abarca <francisca.abarca.p01@mail.pucv.cl>
2023-08-31 23:20:15 -0400

    Agregué nuevos objetos, tales como un arrayList que corresponde a los planes contratados por un cliente.

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,10 +6,13 @@ public class Cliente {
     private String rut;
     private String telefono;
 
+    private ArrayList<String> planesContratados;
+
     public Cliente(String nombre, String rut, String telefono) {
         this.nombre = nombre;
         this.rut = rut;
         this.telefono = telefono;
+        this.planesContratados = new ArrayList<>();
     }
 
     // Getters y setters
@@ -33,7 +36,41 @@ public class Cliente {
         return telefono;
     }
 
+    public ArrayList<String> getPlanesContratados() {
+        return planesContratados;
+    }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public void agregarPlan(String nombrePlan){
+        if(planesContratados.size() >= 10){
+            System.out.print("No se pueden agregar más planes");
+        }
+        else{
+            planesContratados.add(nombrePlan);
+        }
+    }
+
+    public void agregarPlan(String nombrePlan, String planAReemplazar){
+        for(int i = 0 ; i < planesContratados.size() ; i++){
+            if(planesContratados.get(i).equals(planAReemplazar)){
+                planesContratados.set(i, nombrePlan);
+            }
+        }
+    }
+
+    public void eliminarPlan(String nombrePlan) {
+        planesContratados.removeIf(plan -> plan.equals(nombrePlan));
+    }
+
+    public void eliminarPlan(int indice) {
+        if (indice >= 0 && indice < planesContratados.size()) {
+            planesContratados.remove(indice);
+        }
+        else {
+            System.out.println("Índice fuera de rango.");
+        }
     }
 }
