ff68122dd5853f1737fde7bbc0a5dfe6ac769a35
vaguzzely <valentina.guzman.e@mail.pucv.cl>
2023-09-01 00:24:39 -0400

    se han eliminado los atributos y métodos relacionados a los planes.

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,16 +6,13 @@ public class Cliente {
     private String rut;
     private String telefono;
 
-    private Plan planes;
-
     public Cliente(String nombre, String rut, String telefono) {
         this.nombre = nombre;
         this.rut = rut;
         this.telefono = telefono;
-        this.planes = new Plan();
     }
 
-    // Getters y setters
+    // Getters and setters
     public String getNombre() {
         return nombre;
     }
@@ -36,41 +33,7 @@ public class Cliente {
         return telefono;
     }
 
-    public ArrayList<String> getPlanesContratados() {
-        return planesContratados;
-    }
-
     public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public void agregarPlan(String nombrePlan){
-        if(planesContratados.size() >= 10){
-            System.out.print("No se pueden agregar más planes");
-        }
-        else{
-            planesContratados.add(nombrePlan);
-        }
-    }
-
-    public void agregarPlan(String nombrePlan, String planAReemplazar){
-        for(int i = 0 ; i < planesContratados.size() ; i++){
-            if(planesContratados.get(i).equals(planAReemplazar)){
-                planesContratados.set(i, nombrePlan);
-            }
-        }
-    }
-
-    public void eliminarPlan(String nombrePlan) {
-        planesContratados.removeIf(plan -> plan.equals(nombrePlan));
-    }
-
-    public void eliminarPlan(int indice) {
-        if (indice >= 0 && indice < planesContratados.size()) {
-            planesContratados.remove(indice);
-        }
-        else {
-            System.out.println("Índice fuera de rango.");
-        }
     }
 }
